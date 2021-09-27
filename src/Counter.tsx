985bc51703a73a7bba829583dfa499046ed05aea
raverena <raverena96@gmail.com>
2021-09-27 14:09:14 +0900

    [Update] 언제 제네릭을 useState에 쓸지

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  // 상태가 null일 수도 있고 아닐수도 있을때 Generics 를 활용하시면 좋습니다.
   const onIncrease = () => setCount(count + 1);
   const onDecrease = () => setCount(count - 1);
 
